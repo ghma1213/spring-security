e93d793c86603d20c645d1dd6bceeff4648ece18
Gyeonghwan <ghma1213@gmail.com>
2022-08-30 09:46:46 +0900

    구글 로그인 준비

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -32,6 +32,9 @@ public class SecurityConfig {
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
                 .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
         ;
         return http.build();
     }
